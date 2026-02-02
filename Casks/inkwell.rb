1101d4c466141564d3dfd097a99ea7c04f837d9f
outemp <noreply@github.com>
2026-02-01 20:47:36 -0600

    Update inkwell to v1.2.0

--- Casks/inkwell.rb
+++ Casks/inkwell.rb
@@ -1,8 +1,8 @@
 cask "inkwell" do
-  version "1.1.0"
-  sha256 "06ce17f37b32e6163ad351ef6d013a87d9ee7953eb1e5dbaedd9cc66464190d9"
+  version "1.2.0"
+  sha256 "edfcdde8dd04f2a661c08a661507fb08e248f99fc1263fb0cbdfd626658cf071"
 
-  url "https://github.com/outemp/inkwell/releases/download/v#{version}/Inkwell-1.0.0-arm64.dmg"
+  url "https://github.com/outemp/inkwell/releases/download/v#{version}/Inkwell-#{version}-arm64.dmg"
   name "Inkwell"
   desc "Beautiful, minimal Markdown viewer for macOS"
   homepage "https://github.com/outemp/inkwell"
